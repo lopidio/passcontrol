f969f7e21344ac731c0fb776181ca124ece7e8e4
arleudo <antonioarleudo@gmail.com>
2013-08-09 17:23:14 -0300

    tabelas criadas, estrutura diretorio tbm, aquivo xml de cojnfiguração

--- PassControl/src/br/com/thecave/passcontrol/screens/LoginScreen.java
+++ PassControl/src/br/com/thecave/passcontrol/screens/LoginScreen.java
@@ -57,7 +57,14 @@ public class LoginScreen extends javax.swing.JFrame {
 
         lblSenha.setText("Senha:");
 
+        tfSenha.setToolTipText("Insira a senha do usuário!");
+
         jbEntrar.setText("Entrar");
+        jbEntrar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jbEntrarActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -105,40 +112,11 @@ public class LoginScreen extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(LoginScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(LoginScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(LoginScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(LoginScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
+    private void jbEntrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbEntrarActionPerformed
+        performLogin();
+    }//GEN-LAST:event_jbEntrarActionPerformed
 
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new LoginScreen().setVisible(true);
-            }
-        });
-    }
+    
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel jPanel1;
     private javax.swing.JButton jbEntrar;
@@ -147,4 +125,12 @@ public class LoginScreen extends javax.swing.JFrame {
     private javax.swing.JPasswordField tfSenha;
     private javax.swing.JTextField tfUser;
     // End of variables declaration//GEN-END:variables
+
+    
+    public void performLogin()
+    {
+        String user     = tfUser.getText();
+        String senha    = tfSenha.getPassword().toString();
+    }
 }
+
